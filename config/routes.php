04332ff9b41f1f545756d4c5b1c80b857d62a951
邓磊 <denglei@upsec.net>
2026-05-09 12:26:45 +0800

    fix(routes): 将支付提交端点从GET改为POST
    - 修改 /submit.php 路由方法从 GET 到 POST
    - 保持 PaymentController@submit 处理器不变
    - 符合支付安全最佳实践

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Hyperf\HttpServer\Router\Router;
 
 // 易支付 Purchase 端点 (NewAPI go-epay 库默认请求此路径)
-Router::get('/submit.php', 'App\Controller\PaymentController@submit');
+Router::post('/submit.php', 'App\Controller\PaymentController@submit');
 
 // Plisio IPN 回调
 Router::post('/api/callback/plisio', 'App\Controller\PaymentController@callback');
